f69deff66207e4b44ec1f78f830d037d5b9ff16a
Augusto <augustoaraujo2002@gmail.com>
2023-07-25 12:21:41 -0300

    carpetas y archivos con los que trabajare

--- CLIENTE/reactapp/src/App.js
+++ CLIENTE/reactapp/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import Home from "./views/home/home.component"
+import Create from "./views/Create/create.component"
+import Detail from "./views/Detail/detail.component"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
+      <Detail/>
+      <Create/>
     </div>
   );
 }
